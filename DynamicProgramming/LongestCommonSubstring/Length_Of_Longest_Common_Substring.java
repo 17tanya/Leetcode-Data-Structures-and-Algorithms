a0250389855595682120a33f8396704c2952fd65
Tanya Sikarwar <33203848+17tanya@users.noreply.github.com>
2020-03-15 16:08:49 +0530

    Update Length_Of_Longest_Common_Substring.java

--- DynamicProgramming/LongestCommonSubstring/Length_Of_Longest_Common_Substring.java
+++ DynamicProgramming/LongestCommonSubstring/Length_Of_Longest_Common_Substring.java
@@ -6,6 +6,60 @@ A simple solution is to one by one consider all substrings of first string and f
 O(n * m^2)
 
 
+Recursive Approach
+The recursive method for finding longest common substring is:
+Given A and B as two strings, let m as the last index for A, n as the last index for B.
+
+    if A[m] == B[n] increase the result by 1.
+    if A[m] != B[n] :
+      compare with A[m -1] and B[n] or
+      compare with A[m] and B[n -1] 
+    WITH RESULT RESET TO 0.
+!!!!!!!!!    NOTE     !!!!!!!!!
+In Longest Common Subsequence we do not make the result 0. Here we want a contiguous string and so we have to set
+the result to 0 when the characters DO NOT match which is not the case with Longest Common Subsequence.
+*/
+class LCS{
+    String A;
+    String B;
+    LCS(String a, String b){
+        A = a;
+        B = b;
+    }
+    
+    public int LCStr(int n, int m, int count){
+        //we have exhausted one of the strings return the length of the suffix-substring found so far.
+        if(n==0 || m==0) return count;
+        
+        //if the last characters match check for a continous substring in the remaining strings A[1..n-1] and B[1..m-1] 
+        if(A.charAt(n-1) == B.charAt(m-1)){
+            count = LCStr(n-1, m-1, count+1);
+        }
+        
+        /*
+        set count to the maximum length of substring found so far. This piece of code is not in an "else" statement
+        because even when the last characters match which is indicated by the "if" above it does not gurantee that the match
+        will give us the maximum length substring.
+        
+        So, we need to explore other combinations of strings to be find the maximal length substring even when there is a match
+        of the last characters.
+        A[i] == B[j] does not gurantee that A[i-1] == B[j-1] and even when A[i-1] == B[j-1] is true it is not guranteed to be
+        maximum length substring.
+        Also, A[i] != B[j] does not imply (A[i] != B[j-1] or A[i-1] != B[j]). This is possible and might even lead to maximal
+        length substring.
+        when A[i] != B[j] we need to explore other options.
+        */
+        count = Math.max(count,Math.max(LCstr(n, m-1,0), LCStr(n-1, m, 0)));
+        return count;
+        
+        //Notice - Only the call within the "if" block increments count all other calls simply reset count to 0
+        //This is the difference b/w substring and subsequnce problem
+    }
+}
+
+
+
+/*
 Dynamic Programming
 find length of the longest common suffix for all substrings of both strings and store these lengths in a table.
 

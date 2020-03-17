20b6553056c70c1be6bfb1728c9485a85f0b48a4
Tanya Sikarwar <33203848+17tanya@users.noreply.github.com>
2020-03-17 17:55:39 +0530

    Update MinimumSumPartition.java

--- DynamicProgramming/MinimumSumPartition/MinimumSumPartition.java
+++ DynamicProgramming/MinimumSumPartition/MinimumSumPartition.java
@@ -122,3 +122,34 @@ public int minPartition(int A[]){
 Time Complexity - O(n * sum)
 */
 
+//RECURSION USING MEMOIZATION - TOP DOWN
+public static int minPartition(int[] S, int n, int S1, int S2, Map<String, Integer> lookup)
+	{
+		// base case: if list becomes empty, return the absolute
+		// difference between two sets
+		if (n < 0) {
+			return Math.abs(S1 - S2);
+		}
+
+		// construct an unique map key from dynamic elements of the input
+		// Note that can uniquely identify the subproblem with n & S1 only,
+		// as S2 is nothing but S - S1 where S is sum of all elements
+		String key = n + "|" + S1;
+
+		// if sub-problem is seen for the first time, solve it and
+		// store its result in a map
+		if (!lookup.containsKey(key))
+		{
+			// Case 1. include current item in the subset S1 and recur
+			// for remaining items (n - 1)
+			int inc = minPartition(S, n - 1, S1 + S[n], S2, lookup);
+
+			// Case 2. exclude current item from subset S1 and recur for
+			// remaining items (n - 1)
+			int exc = minPartition(S, n - 1, S1, S2 + S[n], lookup);
+
+			lookup.put(key, Integer.min(inc, exc));
+		}
+
+		return lookup.get(key);
+	}

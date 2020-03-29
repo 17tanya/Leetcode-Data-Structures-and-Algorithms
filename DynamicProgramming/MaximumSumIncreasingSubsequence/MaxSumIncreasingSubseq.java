fe677908227ef24521e5d54f3bd98e43d017415a
Tanya Sikarwar <33203848+17tanya@users.noreply.github.com>
2020-03-30 00:36:20 +0530

    Update MaxSumIncreasingSubseq.java

--- DynamicProgramming/MaximumSumIncreasingSubsequence/MaxSumIncreasingSubseq.java
+++ DynamicProgramming/MaximumSumIncreasingSubsequence/MaxSumIncreasingSubseq.java
@@ -15,14 +15,19 @@ public int maxSumOfIncSubseq(int A[]){
     sum[0] = A[0];
     
     for(int i = 1 ; i < n ; i++){
+        //for each index i we consider all elements before it
         for(int j = 0 ; j < i ; j++){
+            
+            //check for increasing subsequence and greater sum
             if(A[j] < A[i] && sum[i] < sum[j]){
                 sum[i] = sum[j];
             }
         }
+        //since the subsequence ends in A[i] add the value of that element as well
         sum[i] += A[i];
     }
     
+    //find max sum
     int maxSum = 0;
     for(int i=0 ; i < n ; i++){
         if(maxSum < sum[i]) maxSum = sum[i];

e4677bc7038443146e4aa018e0c2e72895820e3b
Tanya Sikarwar <33203848+17tanya@users.noreply.github.com>
2020-04-04 23:59:50 +0530

    Update Move_Zeroes.java

--- Arrays/Move_Zeroes.java
+++ Arrays/Move_Zeroes.java
@@ -1,6 +1,8 @@
+//https://leetcode.com/problems/move-zeroes/
+
 class Solution {
     public void moveZeroes(int[] nums) {
-        int cnt = 0;
+        int cnt = 0; 
         int index = 0;
         for(int i=0 ; i < nums.length ; i++){
             if(nums[i] == 0) cnt++;

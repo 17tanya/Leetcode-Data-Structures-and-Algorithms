4a18d7dcea295398972b637460ec74248758a8a8
Tanya Sikarwar <33203848+17tanya@users.noreply.github.com>
2020-04-05 21:20:30 +0530

    Update Vestigium.java

--- Google-Code-Jam-QualificationRound-2020/Vestigium.java
+++ Google-Code-Jam-QualificationRound-2020/Vestigium.java
@@ -1,3 +1,7 @@
+/*
+Problem Link - https://codingcompetitions.withgoogle.com/codejam/round/000000000019fd27/000000000020993c
+*/
+
 import java.util.*;
 
 class Solution{
@@ -13,12 +17,16 @@ class Solution{
             int row = 0;
             int col = 0;
             
+            //calculating trace and checking if each row contains distinct elements
             for(int j=0;j<n;j++){
                 for(int k=0;k<n;k++){
                     arr[j][k] = s.nextInt();
+                    //trace
                     if(j == k) trace += arr[j][k];
+                    //adding all elements of the row to the set
                     set.add(arr[j][k]);
                 }
+                //below condition is true when row contains duplicates
                 if(set.size() != n) row++; 
                 set.clear();
             }
@@ -26,8 +34,10 @@ class Solution{
             set.clear();
             for(int j=0;j<n;j++){
                 for(int k=0;k<n;k++){
+                    //adding all elements of the column to the set
                     set.add(arr[k][j]);
                 }
+                //below condition is true when column contains duplicates
                 if(set.size() != n) col++; 
                 set.clear();
             }

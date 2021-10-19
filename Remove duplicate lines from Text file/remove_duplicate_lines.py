30097efb035716aa301070c946daac53f025ab54
RajanSikarwar <rajansikarwar.ravindra@gmail.com>
2021-10-19 18:52:43 +0530

    Remove duplicate lines from text file

--- Remove duplicate lines from Text file/remove_duplicate_lines.py
+++ Remove duplicate lines from Text file/remove_duplicate_lines.py
@@ -1,6 +1,6 @@
 """
 Replace `sample_file_path.txt` with the location of desired text file to remove duplicate lines(full location path if it is not in the same directory as the code file and only tge name if it is in the same directory as the code file).
-A new text file "clean_text.txt" will be created in the saem directory as the code file with no duplicate lines from `sample_file_path.txt`. 
+A new text file "clean_text.txt" will be created in the saem directory as tge code file with no duplicate lines from `sample_file_path.txt`. 
 """
 
 content = open('sample_file_path.txt','r').readlines()
